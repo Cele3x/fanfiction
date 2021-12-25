f9e1e214a937ebea62ae7d19e25e20128d44a142
jonathan <jonathan.sasse@outlook.de>
2021-12-25 11:14:17 +0100

    add replace_escape_chars input processor and new field definitions for storyCreatedAt and storyUpdatedAt

--- fanfiction/items.py
+++ fanfiction/items.py
@@ -6,29 +6,34 @@
 from scrapy.item import Item, Field
 from scrapy.loader import ItemLoader
 from itemloaders.processors import TakeFirst, MapCompose, Join
-from w3lib.html import remove_tags
+from w3lib.html import remove_tags, replace_escape_chars
+
+# ItemLoader.default_input_processor = MapCompose(remove_tags, replace_escape_chars)
+# ItemLoader.default_outut_processor = TakeFirst()
 
 
 class User(Item):
-    name = Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
-    url = Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    name = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
+    url = Field(output_processor=TakeFirst())
     createdAt = Field(output_processor=TakeFirst())
     updatedAt = Field(output_processor=TakeFirst())
 
 
 class Story(Item):
-    title = Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    title = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
     url = Field(output_processor=TakeFirst())
-    summary = Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
-    status = Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
-    likes = Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
-    follows = Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
-    hits = Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
-    internalId = Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    summary = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
+    status = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
+    likes = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
+    follows = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
+    hits = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
+    storyCreatedAt = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
+    storyUpdatedAt = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
+    internalId = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
     categoryList = Field(output_processor=Join())
-    sourceId = Field(output_processor=TakeFirst())
-    authorUrl = Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
-    genreId = Field(output_processor=TakeFirst())
-    ratingId = Field(output_processor=TakeFirst())
+    sourceId = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
+    authorUrl = Field(output_processor=TakeFirst())
+    genreId = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
+    ratingId = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
     createdAt = Field(output_processor=TakeFirst())
     updatedAt = Field(output_processor=TakeFirst())
