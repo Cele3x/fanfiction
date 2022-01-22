c3c919f2253ce7761b15559bfd57933accc87071
jonathan <jonathan.sasse@outlook.de>
2022-01-22 09:40:17 +0100

    add pause/resume capabilities

--- fanfiction/main.py
+++ fanfiction/main.py
@@ -1,3 +1,7 @@
 from scrapy import cmdline
 
 cmdline.execute('scrapy crawl FanFiktion'.split())
+
+# for pausing and resuming crawls
+# when pausing this crawl, resume it with the same command
+# cmdline.execute('scrapy crawl FanFiktion -s JOBDIR=crawls/FanFiktion-1'.split())
