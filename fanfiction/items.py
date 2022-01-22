9f21bc535475086f07a6409cc07180138ade65f5
jonathan <jonathan.sasse@outlook.de>
2022-01-22 10:49:58 +0100

    add default input processors

--- fanfiction/items.py
+++ fanfiction/items.py
@@ -54,38 +54,40 @@ def replace_multiple_spaces(value: str) -> str:
     return re.sub(r'\s+', ' ', value)
 
 
+DEFAULT_INPUT_PROCESSORS = MapCompose(replace_tags_with_spaces, replace_escape_chars, replace_entities, replace_nbsp, replace_multiple_spaces, str.strip)
+
+
 class User(Item):
-    name = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
+    name = Field(input_processor=DEFAULT_INPUT_PROCESSORS, output_processor=TakeFirst())
     url = Field(output_processor=TakeFirst())
 
 
 class Story(Item):
-    title = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
+    title = Field(input_processor=DEFAULT_INPUT_PROCESSORS, output_processor=TakeFirst())
     url = Field(output_processor=TakeFirst())
-    summary = Field(input_processor=MapCompose(replace_multiple_spaces, str.strip), output_processor=TakeFirst())
-    status = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
-    likes = Field(input_processor=MapCompose(remove_tags, replace_escape_chars, replace_nbsp), output_processor=TakeFirst())
-    follows = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
-    hits = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
-    storyCreatedAt = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
-    storyUpdatedAt = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
-    internalId = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
+    summary = Field(input_processor=DEFAULT_INPUT_PROCESSORS, output_processor=TakeFirst())
+    status = Field(input_processor=DEFAULT_INPUT_PROCESSORS, output_processor=TakeFirst())
+    likes = Field(input_processor=DEFAULT_INPUT_PROCESSORS, output_processor=TakeFirst())
+    follows = Field(input_processor=DEFAULT_INPUT_PROCESSORS, output_processor=TakeFirst())
+    hits = Field(input_processor=DEFAULT_INPUT_PROCESSORS, output_processor=TakeFirst())
+    storyCreatedAt = Field(input_processor=DEFAULT_INPUT_PROCESSORS, output_processor=TakeFirst())
+    storyUpdatedAt = Field(input_processor=DEFAULT_INPUT_PROCESSORS, output_processor=TakeFirst())
     source = Field(output_processor=TakeFirst())
     category = Field(output_processor=TakeFirst())
     topics = Field(output_processor=TakeFirst())
     rating = Field(output_processor=TakeFirst())
     pairing = Field(output_processor=TakeFirst())
-    genre = Field(input_processor=MapCompose(remove_tags, replace_escape_chars, replace_entities), output_processor=TakeFirst())
-    fandom = Field(input_processor=MapCompose(remove_tags, replace_escape_chars, replace_entities), output_processor=TakeFirst())
+    genre = Field(input_processor=DEFAULT_INPUT_PROCESSORS, output_processor=TakeFirst())
+    fandom = Field(input_processor=DEFAULT_INPUT_PROCESSORS, output_processor=TakeFirst())
     authorUrl = Field(output_processor=TakeFirst())
-    characters = Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=Join(', '))
+    characters = Field(input_processor=DEFAULT_INPUT_PROCESSORS, output_processor=Join(', '))
 
 
 class Chapter(Item):
     number = Field(output_processor=TakeFirst())
-    title = Field(output_processor=TakeFirst())
-    content = Field(input_processor=MapCompose(replace_tags_with_spaces, replace_escape_chars, replace_entities, replace_nbsp, replace_multiple_spaces, str.strip), output_processor=TakeFirst())
-    notes = Field(output_processor=TakeFirst())
+    title = Field(input_processor=DEFAULT_INPUT_PROCESSORS, output_processor=TakeFirst())
+    content = Field(input_processor=DEFAULT_INPUT_PROCESSORS, output_processor=TakeFirst())
+    notes = Field(input_processor=DEFAULT_INPUT_PROCESSORS, output_processor=TakeFirst())
     publishedOn = Field(output_processor=TakeFirst())
     reviewedOn = Field(output_processor=TakeFirst())
     storyUrl = Field(output_processor=TakeFirst())
