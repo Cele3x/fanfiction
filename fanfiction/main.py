572b6bc29a908f2569d57ae0efccda8601b094c6
jonathan <jonathan.sasse@outlook.de>
2022-02-01 13:40:31 +0100

    add functionality for pausing/resuming crawls

--- fanfiction/main.py
+++ fanfiction/main.py
@@ -3,20 +3,21 @@ from settings import MONGO_URI, MONGO_DB
 from pymongo import MongoClient
 
 # connect to MongoDB
-client = MongoClient(MONGO_URI)
-db = client[MONGO_DB]
+# client = MongoClient(MONGO_URI)
+# db = client[MONGO_DB]
+#
+# # drop dynamic collections that are not prefilled with seed data
+# static_collections = ['sources', 'genres', 'categories', 'topics', 'ratings', 'pairings']
+# dynamic_collections = db.list_collection_names(filter={'name': {'$nin': static_collections}})
+# for collection in dynamic_collections:
+#     db[collection].drop()
+#
+# # close client
+# client.close()
 
-# drop dynamic collections that are not prefilled with seed data
-static_collections = ['sources', 'genres', 'categories', 'topics', 'ratings', 'pairings']
-dynamic_collections = db.list_collection_names(filter={'name': {'$nin': static_collections}})
-for collection in dynamic_collections:
-    db[collection].drop()
-
-# close client
-client.close()
-
-cmdline.execute('scrapy crawl FanFiktion'.split())
+# cmdline.execute('scrapy crawl FanFiktion'.split())
 
 # for pausing and resuming crawls
 # when pausing this crawl, resume it with the same command
-# cmdline.execute('scrapy crawl FanFiktion -s JOBDIR=crawls/FanFiktion-1'.split())
+# YOU HAVE TO WAIT FOR CLOSING THE JOB WITH JUST ONE CTRL-C (FORCING WON'T WORK)!
+cmdline.execute('scrapy crawl FanFiktion -s JOBDIR=crawls/FanFiktion-1'.split())
