442abaf0dbe61f9be191ca71ec31a9f7d74412b4
jonathan <jonathan.sasse@outlook.de>
2022-02-12 17:48:17 +0100

    add SpiderState extension

--- fanfiction/settings.py
+++ fanfiction/settings.py
@@ -77,9 +77,9 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-# EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-# }
+EXTENSIONS = {
+   'scrapy.extensions.spiderstate.SpiderState': 500,
+}
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
