2ef9d1d9275b1021f87ee9a63a0b8dab09fa87f8
jonathan <jonathan.sasse@outlook.de>
2022-10-10 12:51:27 +0200

    add doc and update command

--- gender-classifier/predict_genders.py
+++ gender-classifier/predict_genders.py
@@ -75,23 +75,31 @@ if __name__ == '__main__':
         if db is None:
             raise Exception('Database connection failed.')
 
+        # load serialized model with weights
         model = load_model('data/gender_classifier.json', 'data/gender_classifier.h5')
 
-        chapters = db.chapters.find({'isTagged': True, 'persons': {'$exists': True}}).limit(10)
+        # find chapters tagged with flair NLP
+        chapters = db.chapters.find({'isTagged': True, 'persons': {'$exists': True}})
+
+        # store previously predicted genders
         predicted_persons = {}
+
         for chapter in chapters:
             females = 0
             males = 0
             indecisives = 0
+
+            # structure {<NAME-1>: <OCCURANCES-NAME-1>, <NAME-2>: <OCCURANCES-NAME-2>, ...}
             for n in chapter['persons'].keys():
-                if n in predicted_persons:
+                if n in predicted_persons:  # use previously predicted value if available
                     gender, probability = predicted_persons[n]
                 else:
                     preprocessed_name = preprocess_name(n)
                     gender, probability = predict_gender(model, preprocessed_name)
                     predicted_persons[n] = [gender, probability]
 
-                if gender and probability and probability > 0.9:
+                # count only with high probability
+                if gender and probability and probability > 0.8:
                     if gender == 'Female':
                         females += 1
                     elif gender == 'Male':
@@ -100,16 +108,26 @@ if __name__ == '__main__':
                         indecisives += 1
                 else:
                     indecisives += 1
+
                 print('Name: %s [%s, %0.2f%%]' % (n, gender, probability))
+
+            # count all predictions with high probability
             total_decisives = females + males
+
+            # count all predictions
             total = total_decisives + indecisives
+
             decisive_percentage = 0.0 if total_decisives == 0 else round(total_decisives / (total_decisives + indecisives), 2)
             female_percentage = 0.0 if total_decisives == 0 else round(females / total, 2)
             male_percentage = 0.0 if total_decisives == 0 else round(males / total, 2)
             ratio = float(females) if males == 0 else round(females / males, 2)
+
+            # result object with counts of occurances, female/male ratio and percentages
             person_genders = {'females': females, 'males': males, 'indecisives': indecisives, 'ratio': ratio,
-                              'decisive_percentage': decisive_percentage, 'female_percentage': female_percentage, 'male_percentage': male_percentage}
-            print(person_genders)
+                              'decisivePct': decisive_percentage, 'femalePct': female_percentage, 'malePct': male_percentage}
+
+            if db.chapters.update_one({'_id': chapter['_id']}, {'$set': {'personGenders': person_genders}}):
+                print('Updated %s with personGender: %s' % (chapter['_id'], person_genders))
     except Exception as e:
         print(e)
     finally:
